5bf7d2441924fbf352d68a7b1abf3c462875fc4c
ArlZ <fernie700@gmail.com>
2026-08-13 22:12:37 +0300

    Register Cloud sync endpoints

--- apps/cloud-api/src/app.module.ts
+++ apps/cloud-api/src/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { ConfigurationModule } from './configuration/configuration.module';
 import { DatabaseModule } from './database/database.module';
+import { SyncModule } from './sync/sync.module';
 import { HealthModule } from './system/health.module';
 
 @Module({
-  imports: [DatabaseModule, HealthModule, ConfigurationModule],
+  imports: [DatabaseModule, HealthModule, ConfigurationModule, SyncModule],
 })
 export class AppModule {}
