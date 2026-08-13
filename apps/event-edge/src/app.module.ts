2bf98f468dd9be980d808f9647f0869c1365fbca
ArlZ <fernie700@gmail.com>
2026-08-13 22:08:04 +0300

    Wire Event Edge sync persistence

--- apps/event-edge/src/app.module.ts
+++ apps/event-edge/src/app.module.ts
@@ -1,5 +1,7 @@
 import { Module } from '@nestjs/common';
+import { EdgeDatabaseModule } from './database/database.module';
+import { SyncModule } from './sync/sync.module';
 import { HealthModule } from './system/health.module';
 
-@Module({ imports: [HealthModule] })
+@Module({ imports: [EdgeDatabaseModule, HealthModule, SyncModule] })
 export class AppModule {}
