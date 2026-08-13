509d1b2f25038539b127cfb91cf148b12100de83
ArlZ <fernie700@gmail.com>
2026-08-13 22:50:53 +0300

    Wire corrected Edge device sync service

--- apps/event-edge/src/sync/sync.module.ts
+++ apps/event-edge/src/sync/sync.module.ts
@@ -5,6 +5,7 @@ import { CloudSyncTransport } from './cloud-sync.transport';
 import { DeviceSyncController } from './device-sync.controller';
 import { DeviceSyncService } from './device-sync.service';
 import { HttpCloudSyncTransport } from './http-cloud-sync.transport';
+import { SafeDeviceSyncService } from './safe-device-sync.service';
 
 @Module({
   controllers: [DeviceSyncController],
@@ -16,12 +17,18 @@ import { HttpCloudSyncTransport } from './http-cloud-sync.transport';
       inject: [EdgeDatabaseService],
     },
     {
+      provide: SafeDeviceSyncService,
+      useFactory: (inner: DeviceSyncService, database: EdgeDatabaseService) =>
+        new SafeDeviceSyncService(inner, database),
+      inject: [DeviceSyncService, EdgeDatabaseService],
+    },
+    {
       provide: CloudForwarderService,
       useFactory: (database: EdgeDatabaseService, transport: CloudSyncTransport) =>
         new CloudForwarderService(database, transport),
       inject: [EdgeDatabaseService, CloudSyncTransport],
     },
   ],
-  exports: [DeviceSyncService, CloudForwarderService],
+  exports: [SafeDeviceSyncService, CloudForwarderService],
 })
 export class SyncModule {}
