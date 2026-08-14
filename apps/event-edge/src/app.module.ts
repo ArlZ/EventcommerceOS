7280edd8ec7b88372e2343aec41a1755a6d03766
ArlZ <fernie700@gmail.com>
2026-08-14 07:11:21 +0300

    Register Event Edge inventory module

--- apps/event-edge/src/app.module.ts
+++ apps/event-edge/src/app.module.ts
@@ -1,7 +1,8 @@
 import { Module } from '@nestjs/common';
 import { EdgeDatabaseModule } from './database/database.module';
+import { InventoryModule } from './inventory/inventory.module';
 import { SyncModule } from './sync/sync.module';
 import { HealthModule } from './system/health.module';
 
-@Module({ imports: [EdgeDatabaseModule, HealthModule, SyncModule] })
+@Module({ imports: [EdgeDatabaseModule, HealthModule, InventoryModule, SyncModule] })
 export class AppModule {}
