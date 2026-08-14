bd93564368a1534a0f29c059fd7754366cdeb7fc
ArlZ <fernie700@gmail.com>
2026-08-14 16:04:13 +0300

    Register card terminal payment providers

--- apps/cloud-api/src/payments/payments.module.ts
+++ apps/cloud-api/src/payments/payments.module.ts
@@ -1,20 +1,28 @@
 import { Module } from '@nestjs/common';
 import { DatabaseModule } from '../database/database.module';
+import { ExternalTerminalProvider } from './external-terminal.provider';
 import { MpesaProvider } from './mpesa.provider';
 import { PaymentAdjustmentsService } from './payment-adjustments.service';
 import { PAYMENT_PROVIDERS } from './payment-provider';
 import { PaymentsController } from './payments.controller';
 import { PaymentsService } from './payments.service';
+import { PesapalSabiProvider } from './pesapal-sabi.provider';
 
 @Module({
   imports: [DatabaseModule],
   controllers: [PaymentsController],
   providers: [
     MpesaProvider,
+    PesapalSabiProvider,
+    ExternalTerminalProvider,
     {
       provide: PAYMENT_PROVIDERS,
-      inject: [MpesaProvider],
-      useFactory: (mpesa: MpesaProvider) => [mpesa],
+      inject: [MpesaProvider, PesapalSabiProvider, ExternalTerminalProvider],
+      useFactory: (
+        mpesa: MpesaProvider,
+        pesapalSabi: PesapalSabiProvider,
+        externalTerminal: ExternalTerminalProvider,
+      ) => [mpesa, pesapalSabi, externalTerminal],
     },
     PaymentsService,
     PaymentAdjustmentsService,
