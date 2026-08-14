95c5e7d9f64adea6f08f52374d455800d6a48a4c
ArlZ <fernie700@gmail.com>
2026-08-14 09:34:30 +0300

    Register Cloud payment module

---
+++ apps/cloud-api/src/payments/payment.module.ts
@@ -0,0 +1,19 @@
+import { Module } from '@nestjs/common';
+import { MpesaDarajaProvider } from './mpesa-daraja.provider';
+import { PaymentController } from './payment.controller';
+import { PAYMENT_PROVIDER } from './payment-provider';
+import { PaymentReconciliationService } from './payment-reconciliation.service';
+import { PaymentService } from './payment.service';
+import { PaymentWebhookService } from './payment-webhook.service';
+
+@Module({
+  controllers: [PaymentController],
+  providers: [
+    PaymentService,
+    PaymentWebhookService,
+    PaymentReconciliationService,
+    { provide: PAYMENT_PROVIDER, useFactory: () => new MpesaDarajaProvider() },
+  ],
+  exports: [PaymentService],
+})
+export class PaymentModule {}
