376fbea869ca467cb365938df534c716db92ad91
ArlZ <fernie700@gmail.com>
2026-08-14 21:21:42 +0300

    Security: wire Edge sync authentication service

--- apps/cloud-api/src/sync/sync.module.ts
+++ apps/cloud-api/src/sync/sync.module.ts
@@ -1,6 +1,10 @@
 import { Module } from '@nestjs/common';
 import { CloudSyncController } from './cloud-sync.controller';
 import { CloudSyncService } from './cloud-sync.service';
+import { EdgeSyncAuthService } from './edge-sync-auth.service';
 
-@Module({ controllers: [CloudSyncController], providers: [CloudSyncService] })
+@Module({
+  controllers: [CloudSyncController],
+  providers: [CloudSyncService, EdgeSyncAuthService],
+})
 export class SyncModule {}
