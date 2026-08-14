aa7b4d863c9525a80a011d0fdc1bb59caf0d4d1f
ArlZ <fernie700@gmail.com>
2026-08-14 09:44:36 +0300

    Wire payment relay into Event Edge

--- apps/event-edge/src/app.module.ts
+++ apps/event-edge/src/app.module.ts
@@ -1,8 +1,9 @@
 import { Module } from '@nestjs/common';
 import { EdgeDatabaseModule } from './database/database.module';
 import { InventoryModule } from './inventory/inventory.module';
+import { PaymentModule } from './payments/payment.module';
 import { SyncModule } from './sync/sync.module';
 import { HealthModule } from './system/health.module';
 
-@Module({ imports: [EdgeDatabaseModule, HealthModule, InventoryModule, SyncModule] })
+@Module({ imports: [EdgeDatabaseModule, HealthModule, InventoryModule, PaymentModule, SyncModule] })
 export class AppModule {}
