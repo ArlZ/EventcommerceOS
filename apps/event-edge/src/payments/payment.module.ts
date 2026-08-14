59f8c2fd45d8ff9903f29145cbec5032fc4b828a
ArlZ <fernie700@gmail.com>
2026-08-14 10:16:20 +0300

    Register Event Edge payment device auth guard

--- apps/event-edge/src/payments/payment.module.ts
+++ apps/event-edge/src/payments/payment.module.ts
@@ -2,6 +2,7 @@ import { Module } from '@nestjs/common';
 import { HttpPaymentCloudTransport } from './http-payment-cloud.transport';
 import { PaymentCloudTransport } from './payment-cloud.transport';
 import { PaymentController } from './payment.controller';
+import { PaymentDeviceAuthGuard } from './payment-device-auth.guard';
 import { PaymentRefreshService } from './payment-refresh.service';
 import { PaymentRelayService } from './payment-relay.service';
 
@@ -10,6 +11,7 @@ import { PaymentRelayService } from './payment-relay.service';
   providers: [
     PaymentRelayService,
     PaymentRefreshService,
+    PaymentDeviceAuthGuard,
     { provide: PaymentCloudTransport, useClass: HttpPaymentCloudTransport },
   ],
   exports: [PaymentRelayService],
