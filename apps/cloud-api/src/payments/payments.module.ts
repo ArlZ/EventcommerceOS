07ce5454a71efab1622aa022fb0e97baa5ed3e6f
ArlZ <fernie700@gmail.com>
2026-08-14 14:46:28 +0300

    Register payment adjustment orchestration

--- apps/cloud-api/src/payments/payments.module.ts
+++ apps/cloud-api/src/payments/payments.module.ts
@@ -1,6 +1,7 @@
 import { Module } from '@nestjs/common';
 import { DatabaseModule } from '../database/database.module';
 import { MpesaProvider } from './mpesa.provider';
+import { PaymentAdjustmentsService } from './payment-adjustments.service';
 import { PAYMENT_PROVIDERS } from './payment-provider';
 import { PaymentsController } from './payments.controller';
 import { PaymentsService } from './payments.service';
@@ -16,7 +17,8 @@ import { PaymentsService } from './payments.service';
       useFactory: (mpesa: MpesaProvider) => [mpesa],
     },
     PaymentsService,
+    PaymentAdjustmentsService,
   ],
-  exports: [PaymentsService],
+  exports: [PaymentsService, PaymentAdjustmentsService],
 })
 export class PaymentsModule {}
