c95104ec0ed305aee57c247a173cf6a6cbaa7cdf
ArlZ <fernie700@gmail.com>
2026-08-14 17:58:08 +0300

    Register cash tender count enrichment

--- apps/cloud-api/src/event-close/event-close.module.ts
+++ apps/cloud-api/src/event-close/event-close.module.ts
@@ -1,5 +1,6 @@
 import { Module } from '@nestjs/common';
 import { DatabaseModule } from '../database/database.module';
+import { EventCloseCashCountService } from './event-close-cash-count.service';
 import { EventCloseController } from './event-close.controller';
 import { EventCloseLedgerService } from './event-close-ledger.service';
 import { EventCloseReportService } from './event-close-report.service';
@@ -8,7 +9,12 @@ import { EventCloseService } from './event-close.service';
 @Module({
   imports: [DatabaseModule],
   controllers: [EventCloseController],
-  providers: [EventCloseLedgerService, EventCloseReportService, EventCloseService],
+  providers: [
+    EventCloseCashCountService,
+    EventCloseLedgerService,
+    EventCloseReportService,
+    EventCloseService,
+  ],
   exports: [EventCloseService],
 })
 export class EventCloseModule {}
