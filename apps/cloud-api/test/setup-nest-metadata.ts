9aa38bf97157dbaa131ecd67b5bd60e9a6c664cf
ArlZ <fernie700@gmail.com>
2026-08-14 19:14:53 +0300

    Reset Cloud security test bypass between tests

--- apps/cloud-api/test/setup-nest-metadata.ts
+++ apps/cloud-api/test/setup-nest-metadata.ts
@@ -1,3 +1,10 @@
+import { afterEach } from 'vitest';
+
 process.env.SECURITY_TEST_BYPASS ??= 'true';
+
+afterEach(() => {
+  process.env.SECURITY_TEST_BYPASS = 'true';
+  delete process.env.SECURITY_TEST_RATE_LIMIT_PER_MINUTE;
+});
 
 import '../src/configuration/injection-metadata';
