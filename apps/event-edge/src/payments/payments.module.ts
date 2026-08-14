97f7f095c1a2655fa2f8a7cdc1219b3002517113
ArlZ <fernie700@gmail.com>
2026-08-14 22:09:56 +0300

    Import POS device authentication into payments

--- apps/event-edge/src/payments/payments.module.ts
+++ apps/event-edge/src/payments/payments.module.ts
@@ -1,11 +1,12 @@
 import { Module } from '@nestjs/common';
 import { EdgeDatabaseModule } from '../database/database.module';
+import { DeviceEdgeAuthModule } from '../security/device-edge-auth.module';
 import { EdgePaymentsController } from './payments.controller';
 import { EdgePaymentsService } from './payments.service';
 import { TerminalPaymentsService } from './terminal-payments.service';
 
 @Module({
-  imports: [EdgeDatabaseModule],
+  imports: [EdgeDatabaseModule, DeviceEdgeAuthModule],
   controllers: [EdgePaymentsController],
   providers: [EdgePaymentsService, TerminalPaymentsService],
   exports: [EdgePaymentsService, TerminalPaymentsService],
