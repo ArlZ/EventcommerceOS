572a69eb4984c375542362aa04102476c4e3a325
ArlZ <fernie700@gmail.com>
2026-08-14 22:42:27 +0300

    Register payment access authorization service

--- apps/cloud-api/src/payments/payments.module.ts
+++ apps/cloud-api/src/payments/payments.module.ts
@@ -3,6 +3,7 @@ import { DatabaseModule } from '../database/database.module';
 import { ExternalTerminalProvider } from './external-terminal.provider';
 import { ManualTerminalService } from './manual-terminal.service';
 import { MpesaProvider } from './mpesa.provider';
+import { PaymentAccessService } from './payment-access.service';
 import { PaymentAdjustmentsService } from './payment-adjustments.service';
 import { PAYMENT_PROVIDERS } from './payment-provider';
 import { PaymentRailService } from './payment-rail.service';
@@ -27,10 +28,17 @@ import { PesapalSabiProvider } from './pesapal-sabi.provider';
       ) => [mpesa, pesapalSabi, externalTerminal],
     },
     PaymentsService,
+    PaymentAccessService,
     PaymentAdjustmentsService,
     ManualTerminalService,
     PaymentRailService,
   ],
-  exports: [PaymentsService, PaymentAdjustmentsService, ManualTerminalService, PaymentRailService],
+  exports: [
+    PaymentsService,
+    PaymentAccessService,
+    PaymentAdjustmentsService,
+    ManualTerminalService,
+    PaymentRailService,
+  ],
 })
 export class PaymentsModule {}
