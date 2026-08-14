6c7a42ade57229af86f17cd0c25006ebe46c2d70
ArlZ <fernie700@gmail.com>
2026-08-14 07:14:15 +0300

    Register Cloud inventory module

--- apps/cloud-api/src/app.module.ts
+++ apps/cloud-api/src/app.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
 import { ConfigurationModule } from './configuration/configuration.module';
 import { DatabaseModule } from './database/database.module';
+import { InventoryModule } from './inventory/inventory.module';
 import { SyncModule } from './sync/sync.module';
 import { HealthModule } from './system/health.module';
 
 @Module({
-  imports: [DatabaseModule, HealthModule, ConfigurationModule, SyncModule],
+  imports: [DatabaseModule, HealthModule, ConfigurationModule, InventoryModule, SyncModule],
 })
 export class AppModule {}
