38ad01a6db5295ee20279b9771e2ebd9dfd55d23
ArlZ <fernie700@gmail.com>
2026-08-14 18:59:03 +0300

    Enable secure-default Event Edge route guard

--- apps/event-edge/src/app.module.ts
+++ apps/event-edge/src/app.module.ts
@@ -2,10 +2,18 @@ import { Module } from '@nestjs/common';
 import { EdgeDatabaseModule } from './database/database.module';
 import { InventoryModule } from './inventory/inventory.module';
 import { EdgePaymentsModule } from './payments/payments.module';
+import { EdgeSecurityModule } from './security/security.module';
 import { SyncModule } from './sync/sync.module';
 import { HealthModule } from './system/health.module';
 
 @Module({
-  imports: [EdgeDatabaseModule, HealthModule, InventoryModule, EdgePaymentsModule, SyncModule],
+  imports: [
+    EdgeDatabaseModule,
+    EdgeSecurityModule,
+    HealthModule,
+    InventoryModule,
+    EdgePaymentsModule,
+    SyncModule,
+  ],
 })
 export class AppModule {}
