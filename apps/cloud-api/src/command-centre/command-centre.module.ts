85dcb6eb03c8514117c720ba485e286d95feee5c
ArlZ <fernie700@gmail.com>
2026-08-14 17:25:57 +0300

    Register command centre device sales aggregation

--- apps/cloud-api/src/command-centre/command-centre.module.ts
+++ apps/cloud-api/src/command-centre/command-centre.module.ts
@@ -1,13 +1,14 @@
 import { Module } from '@nestjs/common';
 import { DatabaseModule } from '../database/database.module';
 import { PaymentsModule } from '../payments/payments.module';
+import { CommandCentreDeviceSalesService } from './command-centre-device-sales.service';
 import { CommandCentreController } from './command-centre.controller';
 import { CommandCentreService } from './command-centre.service';
 
 @Module({
   imports: [DatabaseModule, PaymentsModule],
   controllers: [CommandCentreController],
-  providers: [CommandCentreService],
+  providers: [CommandCentreService, CommandCentreDeviceSalesService],
   exports: [CommandCentreService],
 })
 export class CommandCentreModule {}
