04f6cb6846dc3934ea1350dcd2a29659c646d7eb
ArlZ <fernie700@gmail.com>
2026-08-15 06:55:14 +0300

    Security: make abuse guard composable before authentication

--- apps/cloud-api/src/security/abuse-protection.module.ts
+++ apps/cloud-api/src/security/abuse-protection.module.ts
@@ -1,17 +1,10 @@
 import { Global, Module } from '@nestjs/common';
-import { APP_GUARD } from '@nestjs/core';
 import { AbuseProtectionGuard } from './abuse-protection.guard';
 import { AbuseProtectionService } from './abuse-protection.service';
 
 @Global()
 @Module({
-  providers: [
-    AbuseProtectionService,
-    {
-      provide: APP_GUARD,
-      useClass: AbuseProtectionGuard,
-    },
-  ],
-  exports: [AbuseProtectionService],
+  providers: [AbuseProtectionService, AbuseProtectionGuard],
+  exports: [AbuseProtectionService, AbuseProtectionGuard],
 })
 export class AbuseProtectionModule {}
