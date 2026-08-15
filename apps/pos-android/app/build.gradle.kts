bd062518abbd724fbe6805502325527fd9e10cd3
ArlZ <fernie700@gmail.com>
2026-08-15 18:05:09 +0300

    Migrate Android JVM target to compilerOptions DSL

--- apps/pos-android/app/build.gradle.kts
+++ apps/pos-android/app/build.gradle.kts
@@ -10,9 +10,14 @@ android {
   compileSdk = 35
   defaultConfig { applicationId = "com.eventcommerce.pos"; minSdk = 26; targetSdk = 35; versionCode = 1; versionName = "0.1.0"; testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner" }
   compileOptions { sourceCompatibility = JavaVersion.VERSION_17; targetCompatibility = JavaVersion.VERSION_17 }
-  kotlinOptions { jvmTarget = "17" }
   buildFeatures { compose = true }
   testOptions { unitTests.isIncludeAndroidResources = true }
+}
+
+kotlin {
+  compilerOptions {
+    jvmTarget.set(org.jetbrains.kotlin.gradle.dsl.JvmTarget.JVM_17)
+  }
 }
 
 dependencies {
