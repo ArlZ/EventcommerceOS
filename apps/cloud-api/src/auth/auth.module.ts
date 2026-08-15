ceef4ff50c61901f6c27629ed0dc3f718306dde8
ArlZ <fernie700@gmail.com>
2026-08-15 06:55:36 +0300

    Security: compose global request security in deterministic order

--- apps/cloud-api/src/auth/auth.module.ts
+++ apps/cloud-api/src/auth/auth.module.ts
@@ -1,17 +1,20 @@
 import { Global, Module } from '@nestjs/common';
 import { APP_GUARD } from '@nestjs/core';
 import { DatabaseModule } from '../database/database.module';
+import { AbuseProtectionModule } from '../security/abuse-protection.module';
+import { GlobalSecurityGuard } from '../security/global-security.guard';
 import { OperatorAuthService } from './operator-auth.service';
 import { OperatorIdentityGuard } from './operator-identity.guard';
 
 @Global()
 @Module({
-  imports: [DatabaseModule],
+  imports: [DatabaseModule, AbuseProtectionModule],
   providers: [
     OperatorAuthService,
+    OperatorIdentityGuard,
     {
       provide: APP_GUARD,
-      useClass: OperatorIdentityGuard,
+      useClass: GlobalSecurityGuard,
     },
   ],
   exports: [OperatorAuthService],
