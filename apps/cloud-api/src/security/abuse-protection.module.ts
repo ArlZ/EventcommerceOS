ad62e75c2bf284162a90eb8edfd550a86881f9c0
ArlZ <fernie700@gmail.com>
2026-08-15 06:59:10 +0300

    Security: register Cloud concurrency cap

--- apps/cloud-api/src/security/abuse-protection.module.ts
+++ apps/cloud-api/src/security/abuse-protection.module.ts
@@ -1,10 +1,19 @@
 import { Global, Module } from '@nestjs/common';
+import { APP_INTERCEPTOR } from '@nestjs/core';
+import { AbuseConcurrencyInterceptor } from './abuse-concurrency.interceptor';
 import { AbuseProtectionGuard } from './abuse-protection.guard';
 import { AbuseProtectionService } from './abuse-protection.service';
 
 @Global()
 @Module({
-  providers: [AbuseProtectionService, AbuseProtectionGuard],
+  providers: [
+    AbuseProtectionService,
+    AbuseProtectionGuard,
+    {
+      provide: APP_INTERCEPTOR,
+      useClass: AbuseConcurrencyInterceptor,
+    },
+  ],
   exports: [AbuseProtectionService, AbuseProtectionGuard],
 })
 export class AbuseProtectionModule {}
