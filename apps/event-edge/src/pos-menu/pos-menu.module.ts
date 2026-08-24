09e583417cc655848769716149e44b1b38b0b60b
ArlZ <fernie700@gmail.com>
2026-08-25 01:45:11 +0300

    Register Cloud POS menu pull transport

--- apps/event-edge/src/pos-menu/pos-menu.module.ts
+++ apps/event-edge/src/pos-menu/pos-menu.module.ts
@@ -1,13 +1,14 @@
 import { Module } from '@nestjs/common';
 import { DeviceEdgeAuthModule } from '../security/device-edge-auth.module';
 import { EdgeLocalAdminAuthService } from '../security/edge-local-admin-auth.service';
+import { CloudPosMenuTransport } from './cloud-pos-menu.transport';
 import { PosMenuController } from './pos-menu.controller';
 import { PosMenuService } from './pos-menu.service';
 
 @Module({
   imports: [DeviceEdgeAuthModule],
   controllers: [PosMenuController],
-  providers: [EdgeLocalAdminAuthService, PosMenuService],
+  providers: [CloudPosMenuTransport, EdgeLocalAdminAuthService, PosMenuService],
   exports: [PosMenuService],
 })
 export class PosMenuModule {}
