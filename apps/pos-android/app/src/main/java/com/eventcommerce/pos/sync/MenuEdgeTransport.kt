fdca9b9a741f51adc54b7a721928448563b66e7a
ArlZ <fernie700@gmail.com>
2026-08-24 23:53:04 +0300

    Carry sales-location scope through POS menu transport

--- apps/pos-android/app/src/main/java/com/eventcommerce/pos/sync/MenuEdgeTransport.kt
+++ apps/pos-android/app/src/main/java/com/eventcommerce/pos/sync/MenuEdgeTransport.kt
@@ -1,7 +1,5 @@
 package com.eventcommerce.pos.sync
 
-import com.eventcommerce.pos.domain.MenuCandidate
-
 fun interface MenuEdgeTransport {
-  suspend fun current(): MenuCandidate
+  suspend fun current(): DeliveredMenuSnapshot
 }
