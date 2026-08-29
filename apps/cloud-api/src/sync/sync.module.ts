b70baa896a235c29b82c5ec71f4149cd271ad076
ArlZ <fernie700@gmail.com>
2026-08-30 00:22:28 +0300

    Register Cloud POS roster service

--- apps/cloud-api/src/sync/sync.module.ts
+++ apps/cloud-api/src/sync/sync.module.ts
@@ -2,6 +2,7 @@ import { Module } from '@nestjs/common';
 import { CloudSyncController } from './cloud-sync.controller';
 import { CloudSyncService } from './cloud-sync.service';
 import { EdgeCloudAuthService } from './edge-cloud-auth.service';
+import { PosDeviceRosterService } from './pos-device-roster.service';
 import { PosMenuInstallReceiptService } from './pos-menu-install-receipt.service';
 import { PosMenuPublicationController } from './pos-menu-publication.controller';
 import { PosMenuPublicationService } from './pos-menu-publication.service';
@@ -12,6 +13,7 @@ import { SyncDeviceHealthService } from './sync-device-health.service';
   providers: [
     CloudSyncService,
     EdgeCloudAuthService,
+    PosDeviceRosterService,
     PosMenuInstallReceiptService,
     PosMenuPublicationService,
     SyncDeviceHealthService,
