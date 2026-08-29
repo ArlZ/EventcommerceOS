996290dcdff12f913b36c339e00e125e4588ba0b
ArlZ <fernie700@gmail.com>
2026-08-30 00:24:29 +0300

    Register Command Centre device roster enrichment

--- apps/cloud-api/src/command-centre/command-centre.module.ts
+++ apps/cloud-api/src/command-centre/command-centre.module.ts
@@ -1,6 +1,7 @@
 import { Module } from '@nestjs/common';
 import { DatabaseModule } from '../database/database.module';
 import { PaymentsModule } from '../payments/payments.module';
+import { CommandCentreDeviceRosterService } from './command-centre-device-roster.service';
 import { CommandCentreDeviceSalesService } from './command-centre-device-sales.service';
 import { CommandCentreController } from './command-centre.controller';
 import { CommandCentreService } from './command-centre.service';
@@ -8,7 +9,11 @@ import { CommandCentreService } from './command-centre.service';
 @Module({
   imports: [DatabaseModule, PaymentsModule],
   controllers: [CommandCentreController],
-  providers: [CommandCentreService, CommandCentreDeviceSalesService],
+  providers: [
+    CommandCentreService,
+    CommandCentreDeviceRosterService,
+    CommandCentreDeviceSalesService,
+  ],
   exports: [CommandCentreService],
 })
 export class CommandCentreModule {}
